2dcd801240973802162dd18cfc09a206684d78ab
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-07 22:07:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -21,5 +21,8 @@ class Dog
     end
   end
   
+  def save
+    @@all << self
+  end
   
 end
